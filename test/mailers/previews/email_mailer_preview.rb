1f73fe67016b0bdc1dcd6bf372bf9f4b5a8d28e7
Zaid Huda <tripledot99@gmail.com>
2017-02-09 20:06:09 +0800

    removed email

--- test/mailers/previews/email_mailer_preview.rb
+++ test/mailers/previews/email_mailer_preview.rb
@@ -2,11 +2,11 @@
 class EmailMailerPreview < ActionMailer::Preview
   def default
     EmailMailer.default_email(Email.new(
-      to: "tripledot99@gmail.com,hudadiaz@gmail.com",
-      cc: "another@email.com",
-      bcc: "someone@email.com",
-      subject: "This is a test",
-      content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      to: "person1@email.com",
+      cc: "person2@email.com",
+      bcc: "person3@email.com",
+      subject: "Lorem ipsum dolor sit amet.",
+      content: "Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     ))    
   end
 end
